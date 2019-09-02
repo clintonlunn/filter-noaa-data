31431c1254ecc2b2dc97079a5da629c3a998e9ef
Clinton Lunn <clintonlunn@Clintons-MacBook-Pro.local>
2019-09-01 22:48:47 -0400

    add some options for file output

--- process.js
+++ process.js
@@ -1,22 +1,23 @@
 // process the file that was downloaded in download.js
-
 const {
     opus
 } = require('./fl_ngs/data.js');
 const fs = require('fs-extra');
 
 
-const floridaPIDs = async (data) => { // create an array
+const floridaPIDs = async (data) => { // create an array or json
     const dataArr = data.features;
     const flNGS = await dataArr.filter(item => item.properties.state === "FLORIDA");
     const flPID = flNGS.map(item => item.properties.pid);
 
-    return flNGS;
+    return flNGS; // return this if you want json to be returned
+    // return flPID; // return this if you want array to be returned
 
 }
 
 const writeArray = (arr) => {
-    fs.writeFile("./fl_ngs/fl_ngs.js", JSON.stringify(arr), function (err) {
+    // fs.writeFile("./fl_ngs/fl_ngs.js", JSON.stringify(arr), function (err) { // uncomment this to return an array
+    fs.writeFile("./fl_ngs/fl_ngs.json", JSON.stringify(arr), function (err) { // return a json file
         console.log({
             err
         });
